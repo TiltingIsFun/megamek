a2754d82842d27df8eea194ac9ee64e0f5a797f2
bfontaine <bfontaine706@gmail.com>
2022-04-04 14:49:49 -0400

    some refactors

---
+++ megamek/src/megamek/server/Constants.java
@@ -0,0 +1,10 @@
+package megamek.server;
+
+public class Constants {
+    public static final String DOMINO_EFFECT = "domino effect";
+    public static final String PLAYER = "Player";
+    public static final String CREW_DEATH = "crew death";
+    public static final String ENTITY = ", Entity: ";
+    public static final String ENTITY_NULL = ", Entity was null!";
+    public static final String IMPOSSIBLE_DISPLACEMENT = "impossible displacement";
+}
